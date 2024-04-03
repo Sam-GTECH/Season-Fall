7dbbd7ec12f15d578731019ece533efc5eed9f25
Sam-GTECH <sburton@gaming.tech>
2024-04-03 14:47:09 +0200

    Unity init

---
+++ Assets/Scriptd/BallManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.Design.Serialization;
+using UnityEngine;
+
+public class BallManager : MonoBehaviour
+{
+    private readonly float increase = 0.01f;
+    public float boom = 0;
+    public GameObject scale;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Rigidbody2D rb = GetComponent<Rigidbody2D>();
+            if (rb.IsAwake())
+                rb.Sleep();
+            GetComponent<Transform>().position += new Vector3(0, increase, 0);
+            boom += 0.35f;
+        }
+        else if (Input.GetKeyUp(KeyCode.Space))
+        {
+            Rigidbody2D rb = GetComponent<Rigidbody2D>();
+            if (rb.IsSleeping())
+                rb.WakeUp();
+            rb.AddForce(new Vector2(boom, boom/4));
+            boom = 0;
+        }
+    }
+}

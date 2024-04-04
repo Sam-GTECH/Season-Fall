a46decfb73d610e74d99071deade99cc858866c8
Sam-GTECH <sburton@gaming.tech>
2024-04-04 19:01:47 +0200

    uhhhhhh

---
+++ Assets/Scriptd/feetManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class feetManager : MonoBehaviour
+{
+    public bool isGrounded = false;
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+            isGrounded = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+            isGrounded = false;
+    }
+}

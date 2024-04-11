b5908d8ccdb26956b189c9815625b97a0d4f80b3
Sam-GTECH <sburton@gaming.tech>
2024-04-11 19:27:03 +0200

    Level 3 started

---
+++ Assets/Scripts/CameraFollowY.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowY : MonoBehaviour
+{
+    public float max = 4.4f;
+    public float min = -3.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/Victory.cs
+++ Assets/Scripts/Victory.cs
@@ -10,12 +10,15 @@ public class Victory : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        Debug.Log("Victory for: "+collision.gameObject.tag);
+        if (collision.gameObject.tag != "main")
+            return;
         string title = SceneManager.GetActiveScene().name;
         string[] subs = title.Split(" ");
         bool succ = int.TryParse(subs[1], out int num);
         if (!succ)
-            throw new ArgumentException($"Something went wrong with the name of the current scene. ({title})");
+            throw new ArgumentException($"Something is wrong with the name of the current scene. ({title})");
         Debug.Log(subs[0] + " " + (num + 1));
         SceneManager.LoadScene(subs[0] + " " + (num+1));
     }
-}
+} 
